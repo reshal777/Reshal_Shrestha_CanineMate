3ec8bf1cf85f7eec563cfa63f19029113e9f96d8
Reshal Shrestha <reshalshrestha7@gmail.com>
2026-04-05 16:01:18 +0545

    Remove temporary and utility scripts

--- verify_and_write.py
+++
@@ -1,6 +0,0 @@
-from pets.models import Dog
-import os
-with open('verify_results.txt', 'w') as f:
-    for d in Dog.objects.filter(is_adoptable=True):
-        f.write(f"ID: {d.id}, Name: {d.name}, Image: {d.image.url if d.image else 'EMPTY'}, Is Adoptable: {d.is_adoptable}\n")
-print("Done writing results")
--- verify_dogs.py
+++
@@ -1,3 +0,0 @@
-from pets.models import Dog
-for d in Dog.objects.filter(is_adoptable=True):
-    print(f"ID: {d.id}, Name: {d.name}, Image: {d.image}, Is Adoptable: {d.is_adoptable}")
